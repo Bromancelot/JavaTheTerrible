4531a6b2f502ca91bb36ffc877d61cbe3a114a91
CHC <35937774+SirMcSourTowel@users.noreply.github.com>
2019-02-14 14:17:15 -0500

    Update Ex4-1_FutureValueApp.java

--- Ex4-1_FutureValueApp.java
+++ Ex4-1_FutureValueApp.java
@@ -1,1 +1,54 @@
+import java.util.Scanner;
+import java.text.NumberFormat;
 
+/*
+    CHC-14FEB2019
+        Added debugging statement within the for loop that calculates the future value.
+        Displays the month and future value each time through the loop.
+*/
+
+public class FutureValueApp {
+
+    public static void main(String[] args) {
+        System.out.println("The Future Value Calculator\n");
+        
+        @SuppressWarnings("resource")
+        Scanner sc = new Scanner(System.in);
+        String choice = "y";
+        while (choice.equalsIgnoreCase("y")) {
+            // get the input from the user
+            System.out.print("Enter monthly investment:   ");
+            double monthlyInvestment = sc.nextDouble();
+            System.out.print("Enter yearly interest rate: ");
+            double interestRate = sc.nextDouble();
+            System.out.print("Enter number of years:      ");
+            int years = sc.nextInt();
+
+            // convert yearly values to monthly values
+            double monthlyInterestRate = interestRate / 12 / 100;
+            int months = years * 12;
+
+            // use a for loop to calculate the future value
+            double futureValue = 0.0;
+            for (int i = 1; i <= months; i++) {
+                futureValue = (futureValue + monthlyInvestment) * 
+                              (1 + monthlyInterestRate);
+                System.out.println("Month " + i + ":" + futureValue);
+            }
+
+            // format the result and display it to the user
+            NumberFormat currency = NumberFormat.getCurrencyInstance();
+            System.out.println("Future value:               "
+                    + currency.format(futureValue));
+           
+            System.out.println();
+            
+
+            // see if the user wants to continue
+            System.out.print("Continue? (y/n): ");
+            choice = sc.next();
+            System.out.println();
+        }
+        System.out.println("Deuces!");
+    }
+}
