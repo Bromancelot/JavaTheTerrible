5d3d229a38fc5043218c14b5045580f4a17c9a97
CHC <35937774+SirMcSourTowel@users.noreply.github.com>
2019-02-23 17:36:50 -0500

    Enhance the TestScoreApp

--- Ex4-3_TestScoreApp.java
+++ Ex4-3_TestScoreApp.java
@@ -1,86 +1,13 @@
-    import java.util.Scanner;
+import java.util.Scanner;
     
     /*
       CHC-14FEB2019
-        So on and so forth.
+        Doing the Do(-while)'s
+        Gave user the ability to add more than one set of test scores. User
+            states how many test scores will be entered.
+        Added minimum and maximum test scores to be displayed on output.
+        
     */
-
-    public class TestScoreApp 
-    {  
-         
-    	public static void main(String args[ ]) 
-       {
-    	
-    		Scanner kbd = new Scanner(System.in);
-    	
-    	//total number of test scores possible in CS 200
-    		final int MAX_SIZE = 10;  
-    	//Create (declare & instantiate) an int array of size 10, called: testScore
-    		int [] testScore = new int[MAX_SIZE];
-    	//Declare any other variable storage space needed by the program
-    		int number;
-    		int scoreTotal =0;
-    		int averageScore =0;
-    		int index;
-    		int scoreCount =0;
-    			
-    	/*Initialize the first 7 values to your test scores thus far in this course from user keyboard input 
-    	with a "do-while loop", sentinel value (-1) to stop loop,
-    	and count to keep track of how many values you have assigned to the array. */
-    	do
-    	{
-    		System.out.println("Please enter your test scores. Enter 999 to quit.");
-    		number = kbd.nextInt();
-    		testScore[scoreCount] = number;
-    		
-    		if(testScore[scoreCount] != 999)
-    		{
-    			scoreCount++;
-    		}
-    		}
-    		while(number != -1 && scoreCount < testScore.length);	
-    	
-    	/*Next, use the counter from the loop above to run a 
-    	"for loop" to total your test scores. */
-    	if(scoreCount > 1)
-    	{
-    	
-    		for(index = 0; index < scoreCount; index++)
-    		{
-    			scoreTotal = scoreTotal + testScore[index];
-    		}
-    			if(number == -1)
-    			{
-    	
-    	/*Then, find the average score of your testzes thus far 
-    	using the total divided by the scoreCounter*/
-    			averageScore = scoreTotal / scoreCount;
-    			}
-    			else
-    			{
-    				averageScore = scoreTotal/scoreCount;
-    			}
-    		}
-    	
-    	//Display (echo) your individual scores 
-    		for(index = 0; index < scoreCount; index++)
-    		{			
-    			System.out.print(testScore[index] + " ");
-    		}
-    	
-    	//Display the total number of the scores
-    		System.out.println();
-    		System.out.println("The total of the test scores is: " + scoreTotal);
-    	
-    	//Display the test score average
-    	System.out.println("The average test score is: " + averageScore);				
-      
-      } //closing main method
-    } //closing class header
-
-
-
-import java.util.Scanner;
 
 public class TestScoreApp
 {
@@ -89,37 +16,82 @@ public class TestScoreApp
         // display operational messages
         System.out.println("Please enter test scores that range from 0 to 100.");
         System.out.println("To end the program enter 999.");
-        System.out.println();  // print a blank line
-
+        
         // initialize variables and create a Scanner object
         int scoreTotal = 0;
         int scoreCount = 0;
         int testScore = 0;
+        int maxTestScore = 0;
+        int minTestScore = 100;
+        int numOfScores = 0;
         Scanner sc = new Scanner(System.in);
+        String moreTests = "y";
+        while (!moreTests.equalsIgnoreCase("n"))
+            
+        
+        //System.out.println();  // print a blank line
+        
 
         // get a series of test scores from the user
-        while (testScore != 999)
+        do 
         {
-            // get the input from the user
-            System.out.print("Enter score: ");
-            testScore = sc.nextInt();
+        // ask user how many scores will be entered
+        System.out.println("How many scores test scores will be entered?: ");
+        numOfScores = sc.nextInt();
+        if (numOfScores > 0 && numOfScores != 999)
+        {
+            scoreCount = 0;
+            scoreTotal = 0;
+            while (numOfScores > 0)
+            
+            
+        // get the input from the user
+        System.out.print("Enter score: ");
+        testScore = sc.nextInt();
 
-            // accumulate score count and score total
-            if (testScore <= 100)
+        // accumulate score count and score total
+        if (testScore <= 100)
             {
                 scoreCount = scoreCount + 1;
                 scoreTotal = scoreTotal + testScore;
+                numOfScores --;
+                
+                //Get min and max test scores
+                maxTestScore = Math.max(maxTestScore, testScore);
+                minTestScore = Math.min(minTestScore, testScore);
             }
-            else if (testScore != 999)
-                System.out.println("Invalid entry, not counted");
+            
+            // does not allow user to enter anything greater than 100
+            // or less than 0
+            if (testScore > 100 && testScore != 999 )
+            {
+                System.out.println("Invalid entry, not counted.");
+                System.out.println("Please enter a number between 0-100.");
+                System.out.println("Enter Score: ");
+                testScore = sc.nextInt();
+            }
+            
+            else if (testScore == 999)
+            {
+                System.out.println("Averaging scores.");
+            }
         }
-
+        
+        while (testScore != 999);
+                
         // display the score count, score total, and average score
         double averageScore = scoreTotal / scoreCount;
         String message = "\n" +
                          "Score count:   " + scoreCount + "\n"
                        + "Score total:   " + scoreTotal + "\n"
-                       + "Average score: " + averageScore + "\n";
+                       + "Average score: " + averageScore + "\n"
+                       + "Min Test Score: " + minTestScore + "\n"
+                       + "Max Test Score: " + maxTestScore + "\n";
         System.out.println(message);
+        System.out.println("Enter more test scores? ('y' or 'n'):  ");
+        moreTests = sc.next();
+        System.out.println();
+        }
+     // incomplete. Getting closer.... and pissed   
     }
 }
