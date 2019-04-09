8df4ea012e06493a3867d4a4a22a717db98a25f4
CHC <35937774+SirMcSourTowel@users.noreply.github.com>
2019-04-09 14:26:47 -0400

    Update Ex7-2c_FinancialCalculations.java

--- Ex7-2/Ex7-2c_FinancialCalculations.java
+++ Ex7-2/Ex7-2c_FinancialCalculations.java
@@ -1,1 +1,12 @@
-
+public class FinancialCalculations {
+    public static double calculateFutureValue(double monthlyInvestment,
+            double monthlyInterestRate, int months) {
+        double futureValue = 0;
+        for (int i = 1; i <= months; i++) {
+            futureValue
+                    = (futureValue + monthlyInvestment)
+                    * (1 + monthlyInterestRate);
+        }
+        return futureValue;
+    }    
+}
