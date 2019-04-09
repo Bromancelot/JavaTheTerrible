3bf9e2d5747e6cdd147b4d37dadc406897290b2e
CHC <35937774+SirMcSourTowel@users.noreply.github.com>
2019-04-09 14:54:45 -0400

    Update and rename standard to Ex9-1c_Displayable.java

---
+++ Ex9-1/Ex9-1c_Displayable.java
@@ -0,0 +1,10 @@
+/*
+
+    CHC-09Apr2019
+        Added interface: Displayable
+        Added single method called getDisplayText. Returns a string.
+
+*/
+public interface Displayable {
+	String getDisplayText();
+}
