0efdcf34b25ad93764d67d16b0df14b0fe6197a6
CHC <35937774+SirMcSourTowel@users.noreply.github.com>
2019-04-09 14:56:03 -0400

    Create Ex9-1bDepartmentConstants.java

---
+++ Ex9-1/Ex9-1bDepartmentConstants.java
@@ -0,0 +1,13 @@
+/*
+
+    CHC09APR2019
+        Added an interface named DepartmentConstants.
+
+*/
+
+public interface DepartmentConstants 
+{
+	final int ADMIN = 1;
+	final int EDITORIAL = 2;
+	final int MARKETING = 3;
+}
