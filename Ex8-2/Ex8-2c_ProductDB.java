4286ab36281433754b6a2c74780e535b406caf7a
CHC <35937774+SirMcSourTowel@users.noreply.github.com>
2019-04-09 14:45:14 -0400

    Create Ex8-2c_ProductDB.java

---
+++ Ex8-2/Ex8-2c_ProductDB.java
@@ -0,0 +1,74 @@
+
+import java.util.HashSet;
+
+/*
+    CHC-26Mar2019
+        Modified the class to return a CompactDisc object.
+*/
+
+public class ProductDB
+{
+    public static Product getProduct(String productCode)
+    {
+        // In a more realistic application, this code would
+        // get the data for the product from a file or database
+        // For now, this code just uses if/else statements
+        // to return the correct product data
+
+        Product p = null;
+
+        if (productCode.equalsIgnoreCase("java") ||
+           productCode.equalsIgnoreCase("jsps") ||
+           productCode.equalsIgnoreCase("mcb2"))
+        {
+            Book b = new Book();
+            if (productCode.equalsIgnoreCase("java"))
+            {
+                b.setCode(productCode);
+                b.setDescription("Murach's Beginning Java");
+                b.setPrice(49.50);
+                b.setAuthor("Andrea Steelman");
+            }
+            
+            else if (productCode.equalsIgnoreCase("jsps"))
+            {
+                b.setCode(productCode);
+                b.setDescription("Murach's Java Servlets and JSP");
+                b.setPrice(49.50);
+                b.setAuthor("Andrea Steelman");
+            }
+            
+            else if (productCode.equalsIgnoreCase("mcb2"))
+            {
+                b.setCode(productCode);
+                b.setDescription("Murach's Mainframe COBOL");
+                b.setPrice(59.50);
+                b.setAuthor("Mike Murach");
+            }
+            p = b; // set Product object equal to the Book object
+        }
+        
+        else if (productCode.equalsIgnoreCase("txtp"))
+        {
+            Software s = new Software();
+            s.setCode("txtp");
+            s.setDescription("TextPad");
+            s.setPrice(27.00);
+            s.setVersion("4.7.3");
+            p = s; // set Product object equal to the Software object
+        }
+        
+        else if (productCode.equalsIgnoreCase("bab"))
+        {
+            CompactDisc cd = new CompactDisc();
+            cd.setCode("bab");
+            cd.setDescription("The Beatles: Abby Road");
+            cd.setPrice(14.00);
+            cd.setArtist("The Beatles");
+            p = cd; // set Product object equal to the CD object
+            
+        }
+        
+        return p;
+    }
+}
