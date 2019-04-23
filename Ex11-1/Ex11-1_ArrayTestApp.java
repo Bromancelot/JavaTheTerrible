1e506d7e0774648a0900a441dbbf72d86b818cf4
CHC <35937774+SirMcSourTowel@users.noreply.github.com>
2019-04-23 14:45:34 -0400

    ch11_ex1_ArrayTestApp.java
    Use a one-dimensional array

--- Ex11-1/Ex11-1_ArrayTestApp.java
+++ Ex11-1/Ex11-1_ArrayTestApp.java
@@ -1,7 +1,11 @@
 /*
 
     CHC-21APR2019
-        Created a one-dimensional array 
+        Created a one-dimensional array of 99 double values
+        Used Math.random to add a random number between 0-100
+        Enhanced for loop sums the values. They're then averaged and printed
+        Sort method gets and prints the median(50th value)
+        Prints the 9th value and every 9th value after that.
 
 */
 
@@ -17,12 +21,13 @@ public class ArrayTestApp
     for (int i=0; i < testArray.length; i++)
     testArray[i] = 100.0*Math.random(); 
  
-   //get sum, then print the average
+   //get sum
     double sum = 0.0;
     for(double a: testArray)
     {
         sum += a;
     }
+   //get and print average
     double average = sum/testArray.length; 
     System.out.println("Average is: " + average);
     System.out.println();   
@@ -32,11 +37,10 @@ public class ArrayTestApp
     System.out.println("Median is: " + testArray[testArray.length/2]);
     System.out.println();
  
-   //print the 9th value and every 9th value after
+   //using formatting to print the 9th value and every 9th value after
     for (int i = 9; i < testArray.length; i = i + 9) 
     {
-    //System.out.println("9th values: " + testArray[i]);
-    //printed formatted values because it looks nicer
+    //because it looks nicer
     System.out.printf("Value %2d is: %f%n", i, testArray[i]);
     }
   }
