572cbd483ec1c57b5d7ebaa0d138afb96fc1a30a
CHC <35937774+SirMcSourTowel@users.noreply.github.com>
2019-04-23 14:37:19 -0400

    ch11_ex1_ArrayTestApp.java
    Use a one-dimentional array

--- Ex11-1/Ex11-1_ArrayTestApp.java
+++ Ex11-1/Ex11-1_ArrayTestApp.java
@@ -1,3 +1,10 @@
+/*
+
+    CHC-21APR2019
+        Created a one-dimensional array 
+
+*/
+
 import java.util.Arrays;
  
 public class ArrayTestApp
@@ -10,27 +17,27 @@ public class ArrayTestApp
     for (int i=0; i < testArray.length; i++)
     testArray[i] = 100.0*Math.random(); 
  
- 
- 
    //get sum, then print the average
     double sum = 0.0;
     for(double a: testArray)
     {
         sum += a;
     }
-      double average = sum/testArray.length; 
+    double average = sum/testArray.length; 
     System.out.println("Average is: " + average);
     System.out.println();   
  
-   //sort the values and print the median
+   //sort, then print the median
     Arrays.sort(testArray);
     System.out.println("Median is: " + testArray[testArray.length/2]);
     System.out.println();
  
-  //print the 9th value and every 9th value after
-   for (int i = 8; i < testArray.length; i = i + 9) {
-     System.out.println("9th values: " + testArray[i]);
-     //System.out.printf("Value %2d is: %f%n", i, arrayTest[i]);
+   //print the 9th value and every 9th value after
+    for (int i = 9; i < testArray.length; i = i + 9) 
+    {
+    //System.out.println("9th values: " + testArray[i]);
+    //printed formatted values because it looks nicer
+    System.out.printf("Value %2d is: %f%n", i, testArray[i]);
     }
   }
 }
