ff894c9f41c81898c9b5f0dbd1e9aa1bdf69dbe8
CHC <35937774+SirMcSourTowel@users.noreply.github.com>
2019-04-26 18:36:20 -0400

    Update Ex9-1a_DisplayableTestApp.java

--- Ex9-1/Ex9-1a_DisplayableTestApp.java
+++ Ex9-1/Ex9-1a_DisplayableTestApp.java
@@ -5,13 +5,25 @@ public class DisplayableTestApp
         System.out.println("Welcome to the Displayable Test application\n");
 
         // create an Employee object
-
+        Displayable e = new Employee (2,"Smith","John");
+        
         // display the employee information
+        display(e);
 
         System.out.println();
 
         // create a Product object
-
+        Displayable p = new Product("java", "Murach's Java Programming", 57.50);
+        
         // display the product information
+        display(p);
+        
+        System.out.println(); 
     }
+    
+    private static void display(Displayable d) 
+    {
+        System.out.println(d.getDisplayText());
+    }
+
 }
