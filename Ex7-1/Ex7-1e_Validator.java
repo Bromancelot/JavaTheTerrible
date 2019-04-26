1b19ba270173c22f0d33f76afbbfdec5563bc39d
CHC <35937774+SirMcSourTowel@users.noreply.github.com>
2019-04-26 10:03:11 -0400

    Enhance the Line Item application
    ch07_ex1_LineItem

--- Ex7-1/Ex7-1e_Validator.java
+++ Ex7-1/Ex7-1e_Validator.java
@@ -1,1 +1,110 @@
+import java.util.Scanner;
 
+/*
+    CHC-7Mar2019
+        Modified methods for Validator to be "regular" methods instead
+        of static.
+        Added a constructor that takes zero arguments.
+*/
+
+public class Validator
+{
+
+    public String getString(Scanner sc, String prompt)
+    {
+        System.out.print(prompt);
+        String s = sc.next();  // read user entry
+        sc.nextLine();  // discard any other data entered on the line
+        return s;
+    }
+
+    public int getInt(Scanner sc, String prompt)
+    {
+        int i = 0;
+        boolean isValid = false;
+        while (isValid == false)
+        {
+            System.out.print(prompt);
+            if (sc.hasNextInt())
+            {
+                i = sc.nextInt();
+                isValid = true;
+            }
+            
+            else
+            {
+                System.out.println("Error! Invalid integer value. Try again.");
+            }
+            
+            sc.nextLine();  // discard any other data entered on the line
+        }
+        
+        return i;
+    }
+
+    public int getInt(Scanner sc, String prompt,
+    int min, int max)
+    {
+        int i = 0;
+        boolean isValid = false;
+        while (isValid == false)
+        {
+            i = getInt(sc, prompt);
+            if (i <= min)
+                System.out.println(
+                    "Error! Number must be greater than " + min + ".");
+            else if (i >= max)
+                System.out.println(
+                    "Error! Number must be less than " + max + ".");
+            else
+                isValid = true;
+        }
+        
+        return i;
+    }
+
+    public double getDouble(Scanner sc, String prompt)
+    {
+        double d = 0;
+        boolean isValid = false;
+        while (isValid == false)
+        {
+            System.out.print(prompt);
+            if (sc.hasNextDouble())
+            {
+                d = sc.nextDouble();
+                isValid = true;
+            }
+            
+            else
+            {
+                System.out.println("Error! Invalid decimal value. Try again.");
+            }
+            
+            sc.nextLine();  // discard any other data entered on the line
+        }
+        
+        return d;
+    }
+
+    public double getDouble(Scanner sc, String prompt,
+    double min, double max)
+    {
+        double d = 0;
+        boolean isValid = false;
+        while (isValid == false)
+        {
+            d = getDouble(sc, prompt);
+            if (d <= min)
+                System.out.println(
+                    "Error! Number must be greater than " + min + ".");
+            else if (d >= max)
+                System.out.println(
+                    "Error! Number must be less than " + max + ".");
+            else
+                isValid = true;
+        }
+        
+        return d;
+    }
+}
