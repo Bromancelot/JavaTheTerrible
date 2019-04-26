620f664ebd4cc936037020d29e49c38d125546e9
CHC <35937774+SirMcSourTowel@users.noreply.github.com>
2019-04-26 10:02:25 -0400

    Enhanced the Line Item application
    ch07_ex1_LineItem

--- Ex7-1/Ex7-1d_ProductDB.java
+++ Ex7-1/Ex7-1d_ProductDB.java
@@ -1,1 +1,53 @@
 
+/*
+    CHC-7Mar2019
+        Added TextPad product to class. Designated code is txtp,
+        description "TextPad", and set price is $20.00.
+*/
+
+public class ProductDB
+{
+    public static Product getProduct(String productCode)
+    {
+        // In a more realistic application, this code would
+        // get the data for the product from a file or database
+        // For now, this code just uses if/else statements
+        // to return the correct product data
+
+        // create the Product object
+        Product product = new Product();
+
+        // fill the Product object with data
+        product.setCode(productCode);
+        if (productCode.equalsIgnoreCase("java"))
+        {
+            product.setDescription("Murach's Beginning Java");
+            product.setPrice(49.50);
+        }
+        
+        else if (productCode.equalsIgnoreCase("jsps"))
+        {
+            product.setDescription("Murach's Java Servlets and JSP");
+            product.setPrice(49.50);
+        }
+        
+        else if (productCode.equalsIgnoreCase("mcb2"))
+        {
+            product.setDescription("Murach's Mainframe COBOL");
+            product.setPrice(59.50);
+        }
+        
+        else if (productCode.equalsIgnoreCase("txtp")) 
+        {
+            product.setDescription("TextPad");
+            product.setPrice(20.00);
+	}
+        
+        else
+        {
+            product.setDescription("Unknown");
+        }
+        
+        return product;
+    }
+}
