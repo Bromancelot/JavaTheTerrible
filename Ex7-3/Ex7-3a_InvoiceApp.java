e10a38683209d3fa59e3a898f7028c132e448bb6
CHC <35937774+SirMcSourTowel@users.noreply.github.com>
2019-04-28 14:22:42 -0400

    Update Ex7-3a_InvoiceApp.java

--- Ex7-3/Ex7-3a_InvoiceApp.java
+++ Ex7-3/Ex7-3a_InvoiceApp.java
@@ -1,1 +1,31 @@
+import java.util.Scanner;
+	 
+public class InvoiceApp
+{
+    public static void main(String[] args)
+    {
+        // display a welcome message
+	System.out.println("Welcome to the Invoice Total Calculator");
+	System.out.println();  // print a blank line
+	 
+	Scanner sc = new Scanner(System.in);
+	String choice = "y";
+	while(choice.equalsIgnoreCase("y"))
+	{
+            String customerType = Validator.getString(sc, "Enter customer type (r/c): ");
+	    double subtotal = Validator.getDouble(sc, "Enter subtotal:  ", 0, 10000);
+	    double discountP = Invoice.getDiscountPercent(subtotal, customerType);
+
+	    //Invoice Object
+	    Invoice invoice = new Invoice(customerType, subtotal, discountP);
+	    String m = invoice.getInvoice();
+	    System.out.println();
+	    System.out.println(m);
+            System.out.print("Continue? (y/n): ");
+	    choice = sc.next();
+	    System.out.println();
+	}
+    }  
+}
+
 
